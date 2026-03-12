aee9b3623437623ff96d440b523806fe3a8222cd
nitikashah333 <nitikashah52@gmail.com>
2026-03-12 21:13:17 +0545

    frontendtesting1

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -15,7 +15,7 @@ const UserList = ({ users, onUserDeleted }) => {
   
   return (
     <div>
-      <h2>listofUsers ({users.length})</h2>
+      <h2>list of Users  ({users.length})</h2>
       {users.length === 0 ? (
         <p>No users found</p>
       ) : (
